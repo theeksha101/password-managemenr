8654e56dee4fb9369c68badbeca6b2e3ec4227c6
theeksha101 <deekshaw25@gmail.com>
2021-05-04 22:13:10 +0530

    Declaration of Aggregation pipeline project

---
+++ test.py
@@ -0,0 +1,10 @@
+from Record import Record
+"""
+1. Implement aggregation in mongo 
+2. Implement aggregation in pymongo 
+3. Test using test.py 
+4. If valid implement UI 
+
+
+"""
+print(Record("diksha", "diksha", "").statistics())
